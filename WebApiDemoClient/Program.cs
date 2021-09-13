c4b3f7d6178dfef076212e29959c0c77af807499
Vit Veres <veres.vit@gmail.com>
2021-09-13 10:59:31 +0300

    Added missing files

---
+++ WebApiDemoClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebApiDemoClient.Repositories;
+
+namespace WebApiDemoClient
+{
+    class Program
+    {
+        private static readonly HttpClient Client = new HttpClient();
+        private static readonly string BaseUrl = "https://localhost:5001";
+
+
+        static async Task Main(string[] args)
+        {
+            var client = new EmployeesClient(Client, BaseUrl);
+            var employees = await client.GetEmployeesByIds(new[] { 1, 2, 3, 4, 5, 6, 7 }.ToList());
+
+            Console.WriteLine($"Employees found: {employees.Count}");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
