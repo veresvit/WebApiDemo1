8f01bed118439bd800cdfd8cf2976221d6746d5d
Vit Veres <veres.vit@gmail.com>
2021-09-13 10:20:16 +0300

    Added possible API responses

--- WebApiDemo/Controllers/EmployeesController.cs
+++ WebApiDemo/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Net;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using WebApiDemo.Data.Dtos;
@@ -22,6 +23,7 @@ namespace WebApiDemo.Controllers
 
         // GET: api/Employees
         [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<EmployeeDto>), (int)HttpStatusCode.OK)]
         public IEnumerable<EmployeeDto> GetEmployees([FromQuery] FilterEmployeeDto filter)
         {
             var employees = _repo.GetEmployees(filter);
@@ -31,6 +33,8 @@ namespace WebApiDemo.Controllers
 
         // GET: api/Employees/5
         [HttpGet("{id:int}", Name = "GetEmployeeById")]
+        [ProducesResponseType(typeof(EmployeeDto), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public ActionResult<EmployeeDto> GetEmployeeById([FromRoute] int id)
         {
             var employee = _repo.GetEmployeeById(id);
@@ -42,6 +46,9 @@ namespace WebApiDemo.Controllers
 
         // POST: api/Employees
         [HttpPost]
+        [ProducesResponseType(typeof(EmployeeDto), (int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public ActionResult<EmployeeDto> CreateEmployee([FromBody] CreateEmployeeDto createEmployee)
         {
             var employeeModel = _mapper.Map<Employee>(createEmployee);
@@ -54,6 +61,9 @@ namespace WebApiDemo.Controllers
 
         // PUT: api/Employees/5
         [HttpPut("{id:int}")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public ActionResult UpdateEmployee([FromRoute] int id, [FromBody] UpdateEmployeeDto updateEmployee)
         {
             if (id != updateEmployee.Id) return BadRequest();
@@ -70,6 +80,8 @@ namespace WebApiDemo.Controllers
 
         // DELETE: api/Employees/5
         [HttpDelete("{id:int}")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
         public ActionResult DeleteEmployee([FromRoute] int id)
         {
             var employee = _repo.GetEmployeeById(id);
